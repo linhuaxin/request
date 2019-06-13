1df043d8c321446341639c50d49ff1dcf1388d79
linhuaxin <linhuaxin@foxmail.com>
2019-06-13 22:15:54 +0800

    处理body数据

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { RequestConfig } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
 
 function request(config: RequestConfig) {
   processConfig(config)
@@ -9,11 +10,16 @@ function request(config: RequestConfig) {
 
 function processConfig(config: RequestConfig): void {
   config.url = transformURL(config)
+  config.data = transformRequestData(config)
 }
 
 function transformURL(config: RequestConfig): string {
   const {url, params} = config
   return buildURL(url, params)
+}
+
+function transformRequestData(config: RequestConfig): any {
+  return transformRequest(config)
 }
 
 export default request
