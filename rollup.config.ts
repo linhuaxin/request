14a8e46e23a646d31f790399ea0db015a76013dd
linhuaxin <linhuaxin@foxmail.com>
2020-09-26 04:29:37 +0800

    [build] 1.0.2

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,7 +7,7 @@ import json from 'rollup-plugin-json'
 
 const pkg = require('./package.json')
 
-const libraryName = 'zerone-axios'
+const libraryName = 'zero-ajax'
 
 export default {
   input: `src/index.ts`,
